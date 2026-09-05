e5102fa7f8810d5767a7b77af9e3b39ab81eb657
Amit <cs25_amitkumar.yadav@isbmcoe.com>
2026-09-05 10:20:11 +0530

    Practiced Basic OOP codes

--- oops_basic.java
+++ oops_basic.java
@@ -1,13 +1,18 @@
+
+import java.util.Scanner;
+
+/* 
+//0.
 class Employee { //Making a custom class
-    int id;
+    float id;
     String name;
-    int salary;
+    float salary;
     public void getDetails(){
-        System.out.println("The ID is: " +id);
-        System.out.println("The name is: " +name);
+        System.out.prfloatln("The ID is: " +id);
+        System.out.prfloatln("The name is: " +name);
     }
     public void getSalary(){
-        System.out.println("The slary is: " +salary);
+        System.out.prfloatln("The slary is: " +salary);
     }
 }
 public class oops_basic {
@@ -27,5 +32,165 @@ public class oops_basic {
         Alok.getSalary();
 
 
+    }
+}
+
+
+//1.
+class Employee1{
+    float salary;
+    String name;
+    public void setName(String n){
+        name =n;
+    }
+
+    public float getSalary(){
+        return salary;
+    }
+
+    public String getName(){
+        return name;
+    }
+}
+
+public class oops_basic{
+    public static void main(String[] args) {
+       Employee1 Amit = new Employee1();
+       Amit.salary = 1000;
+       Amit.setName("Amit Kumar Yadav");
+       System.out.prfloatln(Amit.getName());
+       System.out.prfloatln(Amit.getSalary());
+
+    }
+}
+
+
+//2.
+class cellphone{
+    public void ring(){
+        System.out.prfloatln("Ringing...");
+    }
+
+    public void vibrate(){
+        System.out.prfloatln("Vibrating...");
+    }
+
+    public void callFriend(){
+        System.out.prfloatln("Calling Friend...");
+    }
+}
+
+public class oops_basic{
+    public static void main(String [] args){
+        cellphone motorola = new cellphone();
+        motorola.ring();
+        motorola.vibrate();
+        motorola.callFriend();
+    }
+}
+
+
+//3.
+class square{
+    float side;
+
+    public float perimeter(){
+        return side*4;
+    }
+
+    public float area(){
+        return side*side;
+    }
+}
+
+public class oops_basic{
+    public static void main(String[] args) {
+        square sq = new square();
+        sq.side = 1.5f;
+        System.out.println(sq.perimeter());
+        System.out.println(sq.area());
+    }
+}
+
+//4.
+class rectangle{
+    float lenth;
+    float breadth;
+
+    public float perimeter(){
+        return 2*(lenth+breadth);
+    }
+
+    public float area(){
+        return lenth*breadth;
+    }
+}
+
+public class oops_basic{
+    public static void main(String[] args) {
+        rectangle rect = new rectangle();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Give the lenghth of the rectangle:: ");
+        rect.lenth = sc.nextFloat();
+        System.out.println("Give the breadth of the rectangle: ");
+        rect.breadth = sc.nextFloat();
+
+        System.out.println(rect.perimeter());
+        System.out.println(rect.area());
+    }
+}
+
+
+//5.
+class Gengar{
+    public void nightshade(){
+        System.out.println("Gengar is using night shade...");
+    }
+    
+    public void shadowball(){
+        System.out.println("Gengar is using shadow ball...");
+    }
+
+    public void shadowpunch(){
+        System.out.println("Gengar is using shadow punch...");
+    }
+}
+
+public class oops_basic{
+    public static void main(String[] args) {
+        Gengar gng = new Gengar();
+        gng.nightshade();
+        gng.shadowball();
+        gng.shadowpunch();
+    }
+}
+*/
+
+//6.
+class circle{
+    float radius;
+
+    public float diameter(){
+        return 2*radius;
+    }
+
+    public float perimeter(){
+        return 3.14f*2f*radius;
+    }
+
+    public float area(){
+        return 3.14f*radius*radius;
+    }
+}
+
+public class oops_basic{
+    public static void main(String[] args) {
+        circle cr = new circle();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Give the radius: ");
+        cr.radius = sc.nextFloat();
+        System.out.println(cr.diameter());
+        System.out.println(cr.perimeter());
+        System.out.println(cr.area());
     }
 }
